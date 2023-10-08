705d52ce77aba31d0a77e8e754df272b61173508
gochujjang <41176663+gochujjang@users.noreply.github.com>
2023-10-08 15:28:39 +0700

    first commit

---
+++ lib/models/Doctor.dart
@@ -0,0 +1,33 @@
+import 'package:healthypet/models/Service.dart';
+
+class DoctorModel {
+  String name;
+  String image;
+  List<String> services;
+  int distance;
+
+  DoctorModel({
+    required this.name,
+    required this.image,
+    required this.services,
+    required this.distance,
+  });
+}
+
+var doctors = [
+  DoctorModel(
+      name: "Dr. Stone",
+      image: "Dr. Stone.jpg",
+      services: [Service.vaccine, Service.surgery],
+      distance: 10),
+  DoctorModel(
+      name: "Dr. Vanessa",
+      image: "Dr. Stone.jpg",
+      services: [Service.vaccine, Service.surgery],
+      distance: 10),
+  DoctorModel(
+      name: "Dr. Dadang",
+      image: "Dr. Stone.jpg",
+      services: [Service.vaccine, Service.surgery],
+      distance: 10),
+];
